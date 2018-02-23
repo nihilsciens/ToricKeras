a8ac6d4a8f5b0033e47c8ac87726f49882c4275b
Dimmu Borgir <33319318+nihilsciens@users.noreply.github.com>
2018-02-23 14:15:44 +0100

    Update ToricNetwork.py

--- ToricNetwork.py
+++ ToricNetwork.py
@@ -20,6 +20,7 @@ dataset = numpy.loadtxt("ToricValues.txt", delimiter=",")
 testset = numpy.loadtxt("ToricValues_test.txt", delimiter=",")
 # note shape of indata
 size_y = dataset.shape[0]
+size_yt = testset.shape[0]
 size_x = dataset.shape[1]
 border = size_x - 4
 # split into input (X) and output (Y) variables
@@ -32,31 +33,32 @@ Yt = testset[:,border:size_x]
 N = int( -3/4 + math.sqrt(9/16 + (border - 1)/2) )
 width = N + 1
 height = 2*N + 1
-X_i = numpy.empty([size_y, width, height, 1])
-X_it = numpy.empty([size_y, width, height, 1])
-for i in range(size_y):
-	for j in range(width):
-		X_it[i,j,:,0] = Xt[i,j*height:(j+1)*height]
-		X_i[i,j,:,0] = X[i,j*height:(j+1)*height]
+X_i = numpy.empty([size_y, 1, width, height])
+X_it = numpy.empty([size_yt, 1, width, height])
+for j in range(width):
+    for i in range(size_y):
+        X_i[i,0,j,:] = X[i,j*height:(j+1)*height]
+    for i in range(size_yt):
+        X_it[i,0,j,:] = Xt[i,j*height:(j+1)*height]
 
 #########
 # MODEL #
 #########
 # Define model
 model = Sequential()
-model.add(Conv2D(15, kernel_size=[3,3], input_shape=[width,height,1], strides=[1,1], padding='valid', activation='sigmoid'))
-model.add(AveragePooling2D(pool_size=(2,2), strides=2, padding='valid'))
+model.add(Conv2D(15, kernel_size=[2, 3], input_shape=[width, height, 1], strides=[1, 2], padding='valid', activation='relu'))
+model.add(AveragePooling2D(pool_size=(2, 2), strides=2, padding='valid'))
 model.add(Flatten())
 model.add(Dense(N, activation='sigmoid'))
-model.add(Dense(4, activation='sigmoid'))
+model.add(Dense(4, activation='softmax'))
 # Compile model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 ###################
 # BACKPROPAGATION #
 ###################
 # Fit the model
-model.fit(X_i, Y, epochs=500, batch_size=40)
+model.fit(X_i, Y, epochs=1000, batch_size=1000)
 
 ##############
 # EVALUATION #
@@ -72,10 +74,12 @@ print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 predictions = model.predict(X_it)
 # check accuracy
 diff = numpy.rint(predictions) - Yt
-res = numpy.empty([size_y, 1], dtype='str')
-for i in range(size_y):
+res = numpy.empty([size_yt, 1], dtype='str')
+for i in range(size_yt):
     res[i] = 'i'
-    if diff[i,0] == 0 and diff[i,1] == 0:
+    if numpy.argmax(predictions[i,:]) == numpy.argmax(Yt[i,:]):
         res[i] = 'c'
+
 # print predictions
+numpy.set_printoptions(threshold=numpy.inf)
 print(numpy.concatenate([predictions, Yt, res],axis=1))
